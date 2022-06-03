e8495e1ab9db745031d57dc41ce73f5e33260516
LucaFedericoMarty <101400526+LucaFedericoMarty@users.noreply.github.com>
2022-06-03 09:09:40 -0300

    Uint 8 y Cosas Actualizadas

--- Estudiante.sol
+++ Estudiante.sol
@@ -1,53 +1,64 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.10;
 
 contract estudiante 
 {
-    string private nombre;
-    string private apellido;
+    string private _nombre;
+    string private _apellido;
     string private _nombre_completo;
-    string private curso;
-    address private docente;
-    mapping (string => uint) private notas_materias;
+    string private _curso;
+    address private _docente;
+    mapping (string => uint8) private notas_materias;
+    int private _contador = 0; 
+    int private _promedio;
+    int private notas_totales;
 
     // Declaro todas las variables y diccionario necesarios
 
-    constructor (string memory _nombre, string memory _apellido, string memory _curso) 
+    constructor (string memory _nombre_, string memory _apellido_, string memory _curso_) 
     {
-        nombre = _nombre;
-        apellido = _apellido;
-        curso = _curso;
-        docente = msg.sender;
+        _nombre = _nombre_;
+        _apellido = _apellido_;
+        _curso = _curso_;
+        _docente = msg.sender;
 
         // Le pedimos al usuario de este contrato, que ingreso tanto su nombre como apellido y curso
         // Establecemos que el unico que puede setear los valores en el constructor sea quien la haya llamado, en este caso el docente
     }
 
-    function apellidoF() public view returns (string memory)
+    function apellido() public view returns (string memory)
     {
-        return apellido;
+        return _apellido;
 
         // Devolvemos el apellido
     }
 
-    function nombre_completo() public view returns(string memory)
+    function nombre_completo() public view returns (string memory)
     {
-        return string (abi.encodePacked(nombre, apellido));
-        //_nombre_completo = string.concat(nombre,apellido);
+        return string (abi.encodePacked(_nombre, _apellido));
+        //_nombre_completo = string.concat(_nombre,_apellido);
+
+        // Concatenamos los strings de nombre y apellido en uno solo
     }
 
-    function cursoF() public view returns(string memory)
+    function curso() public view returns(string memory)
     {
-        return curso;
+        return _curso;
+
+        // Devolvemos el curso
     }
 
-    function set_nota_materia(uint nota, string memory materia) public 
+    function set_nota_materia(uint8 nota, string memory materia) public 
     {
-        require(msg.sender == docente, "Sola la docente puede poner notas");
+        require(msg.sender == _docente, "Sola la docente puede poner notas");
         notas_materias[materia] = nota;
+        _contador++;
+
+        // Establecemos que el uncio que puede llamar esta funcion sea el que haya establecido los valores de apellido, nombre y curso, que es el docente
+
     }
 
-    function nota_materia(string memory materia) public view returns (uint)
+    function nota_materia(string memory materia) public view returns (uint8)
     {
         return notas_materias[materia];
     } 
@@ -64,4 +75,20 @@ contract estudiante
             return true;
         }  
     }
+
+    function promedio() public view returns (int)
+    {
+        for (int i = 0; i > _contador; i++)
+        {
+            notas_totales = notas_totales + notas_materias[i];
+        }
+
+        _promedio = notas_materias / _contador;
+
+        return _promedio;
+
+        // Checkear lo de LookUpTable
+    }
+
+
 }
